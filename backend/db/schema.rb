178f04992f500348fb9ba52fee4bcd4078b573fb
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-03-23 16:33:46 +0900

    add correct migration to create user model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -15,19 +15,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_070727) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "provider"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "role"
     t.string "first_name"
     t.string "last_name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "email"
+    t.string "provider"
+    t.string "username"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
