2e4ed2b7cd87a7de8968fa948103dfc1a7ffef7e
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-03-23 16:26:46 +0900

    add user model, update schema, update user controller

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def new
     @user = User.new
@@ -18,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+     render json: @user
     else
       render :new, status: :unprocessable_entity
     end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namspace :api do
+  namespace :api do
     namespace :v1 do
       resources :users
     end
