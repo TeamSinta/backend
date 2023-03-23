e11f69023bf9d54fba7c54a8de158494e196434a
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-03-23 15:54:39 +0900

    add namespace and route

---
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :role, :email)
+  end
+
+end
