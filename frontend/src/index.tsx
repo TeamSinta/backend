4ab5516072dee68a9d99cd8d2b19dfef5d817904
WonWonGit <bkn020612@gmail.com>
2023-03-29 14:06:02 +0900

    global style & style color type

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "styles/GlobalStyle";
+import { DefaultTheme } from "styles/StyleType";
 import App from "./App";
 import { store } from "./app/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={DefaultTheme}>
+      <GlobalStyle />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ frontend/src/styles/StyleType.ts
@@ -0,0 +1,19 @@
+const colors = {
+  accentPurple: "#6462F1",
+  palePurple: "#fff500",
+  white: "#ffffff",
+  lightPurple: "#f6f6fb",
+  lightGrey: "#f5f5f5",
+  black: "#121212",
+  secondary: "#737984",
+  red: "#fabbcf",
+  yellow: "#fffabf",
+  green: "#dbfddc",
+  purpleGrey: "rgba(15, 13, 98, .2)",
+};
+
+export type ColorTypes = typeof colors;
+
+export const DefaultTheme = {
+  colors,
+};
