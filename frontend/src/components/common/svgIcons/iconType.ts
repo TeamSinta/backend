8e78259afe5a3c5d94ed5a5cc26716a5e84230a1
WonWonGit <bkn020612@gmail.com>
2023-04-06 16:39:36 +0900

    icon size type

---
+++ frontend/src/components/common/svgIcons/iconType.ts
@@ -0,0 +1,35 @@
+/**
+ * S : small
+ * M : medium
+ * L : large
+ * B : black
+ * W : white
+ */
+
+export interface IIconProps {
+  width: number;
+  height: number;
+  stroke?: string;
+  fill?: string;
+}
+
+export const iconSB = {
+  width: 18,
+  height: 18,
+  stroke: "#121212",
+  fill: "#121212",
+};
+
+export const iconMB = {
+  width: 20,
+  height: 20,
+  stroke: "#121212",
+  fill: "#121212",
+};
+
+export const iconLB = {
+  width: 24,
+  height: 24,
+  stroke: "#121212",
+  fill: "#121212",
+};
