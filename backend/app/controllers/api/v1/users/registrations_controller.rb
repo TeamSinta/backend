8f2827cfc0bc2f0fb98d79831aa49a1f3cbf54ec
Mattias Velamsson <66011769+smackdh@users.noreply.github.com>
2023-04-06 16:31:20 +0900

    modified create user in registrations_controller and tested that it works

--- backend/app/controllers/api/v1/users/registrations_controller.rb
+++ backend/app/controllers/api/v1/users/registrations_controller.rb
@@ -3,6 +3,16 @@
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
+  def create
+    build_resource(user_params)
+
+    if resource.save
+      respond_with resource
+    else
+      respond_with resource.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def respond_with(resource, _opts = {})
@@ -16,5 +26,9 @@ class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
         status: { message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
       }, status: :unprocessable_entity
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
