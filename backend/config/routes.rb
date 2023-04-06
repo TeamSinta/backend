cbf93b8e48b1f7a74c2170bc4676e2b7294240d1
Mattias Velamsson <66011769+smackdh@users.noreply.github.com>
2023-04-06 16:51:55 +0900

    clean up routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-    devise_for :users, path: '', path_names: {
-      sign_in: 'login',
-      sign_out: 'logout',
-      registration: 'signup'
-    },
-    controllers: {
-      sessions: 'api/v1/users/sessions',
-      registrations: 'api/v1/users/registrations'
-    }
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'api/v1/users/sessions',
+    registrations: 'api/v1/users/registrations'
+  }
 
-    namespace :api do
-      namespace :v1 do
-        resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :users
     end
   end
 end
