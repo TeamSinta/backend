72a1d0ad429ddcc617e5c0550c973217a647db09
mohamed shegow <shegowmo@gmail.com>
2023-04-11 14:11:59 -0700

    added in comments

--- frontend/src/components/common/textBtnL/TextBtnL.tsx
+++ frontend/src/components/common/textBtnL/TextBtnL.tsx
@@ -10,6 +10,8 @@ const TextBtnL = (props: TextBtnWrapProps) => {
   const { disable, label } = props;
 
   return (
+
+    // added in this as a comment in case you want to see how the button will look on a smaller div
     // <div style={{ width: "40px" }}>
     <>
       <TextBtnLWrap disable={disable}>
