ab09e8f0483492024a2fc9051e790eea8ad46251
mohamed shegow <shegowmo@gmail.com>
2023-04-14 00:33:53 -0700

    v1 of axios completed, waitinf review

--- frontend/src/utils/httpService.ts
+++ frontend/src/utils/httpService.ts
@@ -2,17 +2,31 @@ import axios  from "axios";
 
 
 const instance = axios.create({
-  baseURL: 'http://api.example.com/',
+  baseURL: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json'
   }
   });
 
-instance.interceptors.response.use(function(config){
+const getRefreshToken = () => {
 
-  return config;
+  };
+
+instance.interceptors.response.use(function(response){
+  return response;
 }, function (error){
-
+  const originalRequest = error.config;
+  if(error.response.status === 401 && !originalRequest._retry){
+    originalRequest._retry = true;
+    const refreshToken = getRefreshToken(); // get refresh token from a cookie
+    return instance.post('/auth/refresh', {refreshToken}).then((response) => {
+      if(response.status === 200){
+        const accessToken = response.data.accessToken
+        originalRequest.headers.Authorization = `JWT ${accessToken}`
+        return instance(originalRequest)
+      }
+    } )
+  }
 return Promise.reject(error)
 
 });
@@ -23,7 +37,7 @@ const getAccessToken = () => {
 
 
 instance.interceptors.request.use(function(config){
-  const accessToken = getAccessToken();;
+  const accessToken = getAccessToken();// get access token from a variable or cookie
   config.headers.Authorization = `JWT ${accessToken}`
   return config;
 }, function (error){
@@ -31,3 +45,5 @@ instance.interceptors.request.use(function(config){
 return Promise.reject(error)
 
 });
+
+export default instance;
