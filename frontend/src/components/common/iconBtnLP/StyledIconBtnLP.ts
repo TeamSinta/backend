582af9a010a8a45da5e46ec3e401bc4b3a3a65b4
Sam Kavanagh <samkav025@gmail.com>
2023-04-15 15:49:23 +0900

    styled lp icon btn

---
+++ frontend/src/components/common/iconBtnLP/StyledIconBtnLP.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { ButtonLayout } from "../button/StyledBtn";
+
+
+export const IconBtnLPWrap = styled(ButtonLayout)`
+    height: 40px;
+    width: 40px;
+    background: ${(props) => props.theme.colors.accentPurple};
+    color: ${(props) => props.theme.colors.white};
+`;
+
+export const IconBtnLPIcon = styled.div``;
