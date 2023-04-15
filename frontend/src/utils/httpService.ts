ef7984b9290abc935061cc0a34e83b246f223975
mohamed shegow <shegowmo@gmail.com>
2023-04-15 14:46:06 -0700

    refactoed google login & axios request

--- frontend/src/utils/httpService.ts
+++ frontend/src/utils/httpService.ts
@@ -1,64 +1,59 @@
-import axios  from "axios";
+import axios, { AxiosInstance , AxiosError, InternalAxiosRequestConfig } from 'axios';
 
-const instance = axios.create({
-  baseURL: 'http://localhost:3000',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+import { useEffect } from 'react';
+
+
+const HttpService = (): AxiosInstance => {
+  const instance = axios.create({
+    baseURL: process.env.REACT_URL,
   });
 
-const getRefreshToken = ''
-
-const getAccessToken = () => {
-  const name = "access_token=";
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookieArray = decodedCookie.split(";");
-  for (let i = 0; i < cookieArray.length; i++) {
-    let cookie = cookieArray[i];
-    while (cookie.charAt(0) === " ") {
-      cookie = cookie.substring(1);
+  // Request Handler
+  const requestHandler = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig | Promise<InternalAxiosRequestConfig> => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      switch (config.url) {
+        case process.env.APP_LOGIN:
+          return config;
+        default:
+          if (config.headers) {
+            config.headers.Authorization = `Bearer ${token}`;
+          }
+          return config;
+      }
     }
-    if (cookie.indexOf(name) === 0) {
-      return cookie.substring(name.length, cookie.length);
-    }
-  }
-  return null;
+    return config;
+  };
 
+  // Request Error Handler
+  const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+    console.error(`[request error] [${JSON.stringify(error)}]`);
+    return Promise.reject(error);
+  };
+
+  // Response Handler
+  const responseHandler = (response: any) => {
+    return response;
+  };
+
+  // Response Error Handler
+  const onResponseError = (error: AxiosError): Promise<AxiosError> => {
+    console.error(`[response error] [${JSON.stringify(error)}]`);
+    return Promise.reject(error);
+  };
+
+
+  useEffect(() => {
+    const requestInterceptor = instance.interceptors.request.use(requestHandler, onRequestError);
+    const responseInterceptor = instance.interceptors.response.use(responseHandler, onResponseError);
+
+    return () => {
+      instance.interceptors.request.eject(requestInterceptor);
+      instance.interceptors.response.eject(responseInterceptor);
+    };
+  });
+
+  return instance;
 };
 
-
-instance.interceptors.response.use(function(response){
-  return response;
-  }, function (error){
-  const originalRequest = error.config;
-  if(error.response.status === 401 && !originalRequest._retry){
-    originalRequest._retry = true;
-    debugger
-    const refreshToken = getRefreshToken; // get refresh token from a cookie
-    return instance.post('/auth/refresh', {refreshToken}).then((response) => {
-      if(response.status === 200){
-        const accessToken = response.data.accessToken
-        // setAccessToken(accessToken); <- Optional? // store new access token in a variable
-        originalRequest.headers.Authorization = `Bearer ${accessToken}`
-        return instance(originalRequest)
-      }
-    } )
-  }
-return Promise.reject(error)
-
-});
-
-
-
-instance.interceptors.request.use(function(config){
-  const accessToken = getAccessToken();// get access token from a variable or cookie
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }  return config;
-}, function (error){
-
-return Promise.reject(error)
-
-});
-
-export default instance;
+export default HttpService;
--- frontend/src/utils/useGoogleLogin.tsx
+++ frontend/src/utils/useGoogleLogin.tsx
@@ -1,27 +1,31 @@
 import { useGoogleLogin } from "@react-oauth/google";
-import httpService from '../utils/httpService'
+import { useCookies } from 'react-cookie';
+import HttpService from "./httpService";
+
 
 type GoogleLoginReturnType = {
   signIn?: () => void;
 };
 
 const GoogleLogin = (): GoogleLoginReturnType => {
+
+  const [, setCookies] = useCookies(['access_token', 'refresh_token']);
+  const instance = HttpService();
+
+
   const signIn   = useGoogleLogin({
     flow: "auth-code",
     onSuccess: async (codeResponse) => {
       console.log(codeResponse);
-
-      const tokens = await httpService.post("http://localhost:3000/auth/google", {
+      const tokens = await instance.post("http://localhost:3000/auth/google", {
         code: codeResponse.code,
       });
       console.log(tokens);
-
       // Extract the access token and refresh token from the response
       const { access_token, refresh_token } = tokens.data;
-
       // Store the access token and refresh token in cookies or local storage
-      document.cookie = `access_token=${access_token}`;
-      document.cookie = `refresh_token=${refresh_token}`;
+      setCookies('access_token', access_token);
+      setCookies('refresh_token', refresh_token);
     },
     onError: (errorResponse) => console.log(errorResponse),
   });
