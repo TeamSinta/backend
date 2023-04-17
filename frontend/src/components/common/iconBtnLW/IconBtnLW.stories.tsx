45cf7b947f5812b382f2230c3144f0490e418014
Mattias <mattiaswelamsson@gmail.com>
2023-04-17 10:31:07 +0900

    prettified

--- frontend/src/components/common/iconBtnLW/IconBtnLW.stories.tsx
+++ frontend/src/components/common/iconBtnLW/IconBtnLW.stories.tsx
@@ -1,77 +1,76 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import {
-    ArrowDownIcon,
-    CalendarIcon,
-    CandidateIcon,
-    CloseIcon,
-    EditIcon,
-    PlusIcon,
-    SettingIcon,
+  ArrowDownIcon,
+  CalendarIcon,
+  CandidateIcon,
+  CloseIcon,
+  EditIcon,
+  PlusIcon,
+  SettingIcon,
 } from "../svgIcons/Icons";
 import { iconLB } from "../svgIcons/iconType";
 import IconBtnLW from "./IconBtnLW";
 
 const icons = {
-    ArrowDown: <ArrowDownIcon {...iconLB} />,
-    Calendar: <CalendarIcon {...iconLB} />,
-    Candidate: <CandidateIcon {...iconLB} />,
-    Close: <CloseIcon {...iconLB} />,
-    Edit: <EditIcon {...iconLB} />,
-    Plus: <PlusIcon {...iconLB} />,
-    Setting: <SettingIcon {...iconLB} />,
+  ArrowDown: <ArrowDownIcon {...iconLB} />,
+  Calendar: <CalendarIcon {...iconLB} />,
+  Candidate: <CandidateIcon {...iconLB} />,
+  Close: <CloseIcon {...iconLB} />,
+  Edit: <EditIcon {...iconLB} />,
+  Plus: <PlusIcon {...iconLB} />,
+  Setting: <SettingIcon {...iconLB} />,
 };
 
-
 export default {
-    title: "common/IconButtonLW",
-    component: IconBtnLW,
-    argTypes: {
-        icon: {
-            control: {
-                type: "select",
-                options: Object.keys(icons),
-                mapping: {
-                    icons,
-                },
-            },
-            description: "Icon components",
+  title: "common/IconButtonLW",
+  component: IconBtnLW,
+  argTypes: {
+    icon: {
+      control: {
+        type: "select",
+        options: Object.keys(icons),
+        mapping: {
+          icons,
         },
-        disable: {
-            control: {
-                type: "boolean",
-            },
-            description: "Disable or able",
-        }
+      },
+      description: "Icon components",
     },
+    disable: {
+      control: {
+        type: "boolean",
+      },
+      description: "Disable or able",
+    },
+  },
 } as ComponentMeta<typeof IconBtnLW>;
 
 const Template: ComponentStory<typeof IconBtnLW> = (arg) => {
-    var icon = arg.icon;
-    if (icon !== undefined) {
-        icon = icons[arg.icon.toString() as keyof typeof icons];
-    }
+  var icon = arg.icon;
+  if (icon !== undefined) {
+    icon = icons[arg.icon.toString() as keyof typeof icons];
+  }
 
-    if (icon === undefined) {
-        return <IconBtnLW {...arg} />;
-    } else {
-        var current = {
-            ...arg,
-            icon: icon,
-        };
-        return <IconBtnLW {...current} />;
-    }
+  if (icon === undefined) {
+    return <IconBtnLW {...arg} />;
+  } else {
+    var current = {
+      ...arg,
+      icon: icon,
+    };
+    return <IconBtnLW {...current} />;
+  }
 };
 
 export const Primary = Template.bind({});
 
 Primary.args = {
-    icon: <EditIcon {...iconLB} />,
-    disable: false,
+  icon: <EditIcon {...iconLB} />,
+  disable: false,
 };
 
 export const Disable = Template.bind({});
 
 Disable.args = {
-    icon: <EditIcon {...iconLB} />,
-    disable: true,
+  icon: <EditIcon {...iconLB} />,
+  disable: true,
 };
--- frontend/src/components/common/iconBtnLW/IconBtnLW.tsx
+++ frontend/src/components/common/iconBtnLW/IconBtnLW.tsx
@@ -2,21 +2,21 @@ import { ButtonShadow } from "../button/StyledBtn";
 import { IconBtnLWIcon, IconBtnLWWrap } from "./StyledIconBtnLW";
 
 interface IIconBtnLWProps {
-    icon: JSX.Element;
-    disable: boolean;
+  icon: JSX.Element;
+  disable: boolean;
 }
 
 const IconBtnLW = (props: IIconBtnLWProps) => {
-    const { icon, disable} = props;
+  const { icon, disable } = props;
 
-    return (
-        <>
-            <IconBtnLWWrap disable={disable} >
-                <IconBtnLWIcon>{icon}</IconBtnLWIcon>
-                <ButtonShadow />
-            </IconBtnLWWrap>
-        </>
-    );
+  return (
+    <>
+      <IconBtnLWWrap disable={disable}>
+        <IconBtnLWIcon>{icon}</IconBtnLWIcon>
+        <ButtonShadow />
+      </IconBtnLWWrap>
+    </>
+  );
 };
 
 export default IconBtnLW;
