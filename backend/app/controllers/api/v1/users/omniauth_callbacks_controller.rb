9eef57ee89327bd0d618179410e14d15ad4e1c72
Mattias <mattiaswelamsson@gmail.com>
2023-05-14 16:12:44 +0900

    added small methods to controller

--- backend/app/controllers/api/v1/users/omniauth_callbacks_controller.rb
+++ backend/app/controllers/api/v1/users/omniauth_callbacks_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::Users::OmniAuthCallbacksController < Devise::OmniAuthCallbacksController
+  def google_oauth2
+    puts "Omniauth Controller saying HI"
+  end
+
+
+  def failure
+    puts "Something went wrong....?"
+  end
 
 end
