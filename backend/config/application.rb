1284156c1a5c8e646cdbc0e48d5b65e016227a37
Mattias <mattiaswelamsson@gmail.com>
2023-05-15 03:49:20 +0900

    fixed typo and added current_user

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -25,6 +25,5 @@ module Backend
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore, key: '_sinta_session'
-
   end
 end
