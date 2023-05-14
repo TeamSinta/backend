8424867f1832162adaac8ff5e710ef84a93a61b6
Mattias <mattiaswelamsson@gmail.com>
2023-05-14 16:20:24 +0900

    add settings for devise in user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: self
+         :omniauthable, :jwt_authenticatable,
+         omniauth_providers: [:google_oauth2],
+         jwt_revocation_strategy: JwtDenylist
 end
