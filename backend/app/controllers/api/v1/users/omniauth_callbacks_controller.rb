15cc0a896b996786b953643b446921d2c43d6120
Mattias <mattiaswelamsson@gmail.com>
2023-05-15 18:35:47 +0900

    finished refresh token logic

--- backend/app/controllers/api/v1/users/omniauth_callbacks_controller.rb
+++ backend/app/controllers/api/v1/users/omniauth_callbacks_controller.rb
@@ -3,26 +3,38 @@ class Api::V1::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksCon
     puts "\nGoogle Oauth2 Controller Reached 👌Passing over to create a user..."
     code = params[:code]
     @user = User.from_omniauth(code)
-    # puts "User Object: #{@user.inspect}"
 
-    puts "Found user info, now back at Controller....."
+    puts 'Found user info, now back at Controller.....'
 
     if @user.persisted?
       puts "#{@user.name} is logged in"
 
-      payload ={
-        email: @user.email,
-        name: @user.name
+      access_token, = Warden::JWTAuth::UserEncoder.new.call(@user, :user, nil)
+
+      render json: {
+        accessToken: access_token,
+        refreshToken: @user.refresh_tokens.last.token,
+        user: @user
       }
-
-    access_token, _ = Warden::JWTAuth::UserEncoder.new.call(@user, :user, nil)
-
-     render json: {
-      accessToken: access_token,
-      refreshToken: refresh_token.token,
-      user: @user}
     else
       puts 'user login failed'
+    end
+  end
+
+  def refresh_jwt
+    refresh_token = params[:refresh_token]
+
+    tokens = OauthService.refresh_access_token(refresh_token)
+
+    user = User.joins(:refresh_tokens).find_by(refresh_tokens: { token: refresh_token })
+
+    if user
+      # Gen new token
+      jwt, = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil)
+
+      render json: { accessToken: jwt, refreshToken: refresh_token }
+    else
+      render json: { error: 'Invalid refresh token' }
     end
   end
 
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/current_user', to: 'api/v1/users/current_user#index'
+
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -14,7 +15,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       devise_scope :user do
-        get '/users/auth/google_oauth2/callback', to: 'users/omniauth_callbacks#google_oauth2'
+          namespace :users do
+          get '/auth/google_oauth2/callback', to: 'omniauth_callbacks#google_oauth2'
+          post '/refresh_token', to: 'omniauth_callbacks#refresh_jwt'
+        end
       end
     end
   end
