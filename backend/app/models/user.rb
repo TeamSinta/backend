fdec1accb34ea274b5e46679b709722d2c3e6817
Mattias <mattiaswelamsson@gmail.com>
2023-05-15 16:57:33 +0900

    updated refresh token logic

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -18,8 +18,11 @@ class User < ApplicationRecord
       user_info = JWT.decode(access_token['id_token'], nil, false)[0]
       user = User.find_or_create_by!(email: user_info['email']) do |user|
         user.name = user_info['name']
+        user.email = user_info['email']
         user.password = Devise.friendly_token[0, 20]
+        user.password = "password"
       end
+      user.refresh_tokens.create(token: access_token['refresh_token'])
       user
     else
       puts "There's no access token to handle."
