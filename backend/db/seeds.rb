c40a4f95a918e119ebbb008fd89fc534fba8f0e1
Mattias <mattiaswelamsson@gmail.com>
2023-05-19 15:17:19 +0900

    added user profile photo to be included on acc creation, and added seeds for easier testing

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,22 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+# Clear DB
+RefreshToken.delete_all
+puts "All refresh tokens deleted."
 
-
-User.create(
-  [{
-    first_name: "Mohamed",
-    last_name: "Shegow",
-    role: "BDR",
-    email: "shegowmo@gmail.com",
-  }]
-)
-
-User.create(
-  [{
-    first_name: "Mattias",
-    last_name: "ToCool-Velamsson",
-    role: "LGTM",
-    email: "LOVE@IKEA.COM",
-  }]
-)
+User.delete_all
+puts "All users deleted."
