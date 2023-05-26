229e4451f114b88c01f34543acd100012c8e39d8
Mattias <mattiaswelamsson@gmail.com>
2023-05-26 12:00:24 +0900

    updated rubocop config

--- backend/app/controllers/api/v1/users/sessions_controller.rb
+++ backend/app/controllers/api/v1/users/sessions_controller.rb
@@ -2,15 +2,24 @@
 class Api::V1::Users::SessionsController < ApplicationController
   def update_token
     refresh_token = request.headers['Authorization'].split('Bearer ').last
-    user = User.joins(:refresh_tokens)
-               .where(refresh_tokens: {
-                        token: refresh_token,
-                        expiration_date: Time.now..Float::INFINITY
-                      }).first
+    user =
+      User
+        .joins(:refresh_tokens)
+        .where(
+          refresh_tokens: {
+            token: refresh_token,
+            expiration_date: Time.now..Float::INFINITY
+          }
+        )
+        .first
 
     if user
       user.refresh_tokens.find_by(token: refresh_token).destroy
-      new_refresh_token = user.refresh_tokens.create!(token: SecureRandom.urlsafe_base64, expiration_date: Time.now + 1.month)
+      new_refresh_token =
+        user.refresh_tokens.create!(
+          token: SecureRandom.urlsafe_base64,
+          expiration_date: Time.now + 1.month
+        )
       jwt, = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil)
 
       render json: { accessToken: jwt, refreshToken: new_refresh_token.token }
@@ -20,17 +29,23 @@ class Api::V1::Users::SessionsController < ApplicationController
   end
 
   def logout
-
     refresh_token = request.headers['Authorization'].split('Bearer ').last
 
-
-    user = User.joins(:refresh_tokens).find_by(refresh_tokens: { token: refresh_token })
+    user =
+      User.joins(:refresh_tokens).find_by(
+        refresh_tokens: {
+          token: refresh_token
+        }
+      )
 
     if user
       user.refresh_tokens.find_by(token: refresh_token).destroy
-      render json: { user: 'User token deleted, so user is logged out' }, status: :ok
+      render json: {
+               user: 'User token deleted, so user is logged out'
+             },
+             status: :ok
     else
-      raise ApiException::Unauthorized.new("No user session found.")
+      raise ApiException::Unauthorized.new('No user session found.')
     end
   end
 end
