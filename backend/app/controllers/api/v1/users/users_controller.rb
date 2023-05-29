2bcf5f88ca8327bacd87415874be268385f63f22
Mattias <mattiaswelamsson@gmail.com>
2023-05-29 11:06:20 +0900

    refactored oauth service and added rescue in user_controller

--- backend/app/controllers/api/v1/users/users_controller.rb
+++ backend/app/controllers/api/v1/users/users_controller.rb
@@ -1,6 +1,7 @@
 # API controller for managing user profiles, information retrieval, and account deletion.
 # Requires user authentication for profile and destroy actions.
 class Api::V1::Users::UsersController < ApplicationController
+  rescue_from JWT::DecodeError, with: :render_unauthorized
   before_action :authenticate_user!, only: %i[profile destroy]
 
   def profile
