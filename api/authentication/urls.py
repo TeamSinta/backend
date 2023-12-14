dfeb36207143399c46ea83fcaa9260c141fd600d
Mattias Velamsson <smackdh@gmail.com>
2023-12-15 08:54:17 +0900

    add mock login

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -1,12 +1,17 @@
+import os
+
 from dj_rest_auth.views import LogoutView
-from django.conf import settings
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from .views import GoogleLogin, MockGoogleLogin
 
+environment = os.environ.get("ENVIRONMENT")
 urlpatterns = [
-    path("login/", MockGoogleLogin.as_view() if settings.DEBUG else GoogleLogin.as_view(), name="auth_login"),
+    path("mocklogin/", MockGoogleLogin.as_view(), name="mock_auth_login")
+    if environment == "dev"
+    else path("", lambda request: None),
+    path("login/", GoogleLogin.as_view(), name="auth_login"),
     path("logout/", LogoutView.as_view(), name="auth_logout"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),  # potentially move this to another app.
     path("token/verify/", TokenVerifyView.as_view(), name="token/verify"),
--- api/authentication/views.py
+++ api/authentication/views.py
@@ -11,35 +11,39 @@ from company.models import Company
 from user.models import CustomUser, Role, UserCompanies
 
 
-# API receives a authorization code from FE and then goes on with creating/authenticating the user.
 class GoogleLogin(SocialLoginView):
-    # authentication_classes = ()
-    # permission_classes = ()
-    print("auth google login")
+    authentication_classes = ()
+    permission_classes = ()
     adapter_class = GoogleOAuth2Adapter
     callback_url = os.environ.get("FRONTEND_CALLBACK_URL")
-    print(callback_url)
     client_class = OAuth2Client
 
 
 class MockGoogleLogin(LoginView):
     def post(self, request, *args, **kwargs):
-        print("test mocklogin")
         # Create a mock user or retrieve an existing one
+        username = os.environ.get("MOCK_USERNAME", "mockuser")
+        email = os.environ.get("MOCK_EMAIL", "mockuser@example.com")
+        first_name = os.environ.get("MOCK_FIRST_NAME", "Mock")
+        last_name = os.environ.get("MOCK_LAST_NAME", "Mockers")
+        company_name = os.environ.get("MOCK_COMPANY_NAME", "Mock Company")
+        role = os.environ.get("MOCK_ROLE", "admin")
+
         user, created = CustomUser.objects.get_or_create(
-            username="mockuser",
+            username=username,
             defaults={
-                "email": "mockuser@example.com",
+                "email": email,
                 "is_active": True,
-                "first_name": "Mock",
-                "last_name": "Person",
+                "first_name": first_name,
+                "last_name": last_name,
                 "profile_picture": "https://ak.picdn.net/contributors/436585/avatars/thumb.jpg?t=5674227",
             },
         )
-        company, created = Company.objects.get_or_create(name="Mockkers")
-        role, created = Role.objects.get_or_create(name="admin")
+        company, created = Company.objects.get_or_create(name=company_name)
+        role, created = Role.objects.get_or_create(name=role)
         UserCompanies.objects.get_or_create(user=user, company=company, defaults={"role": role})
         user.save()
+
         refresh = RefreshToken.for_user(user)
 
         return Response({"access": str(refresh.access_token), "refresh": str(refresh)})
