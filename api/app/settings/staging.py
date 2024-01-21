7897b5d7517d4b9a9b7fe6e696ad3e99d25fa0aa
Mattias Velamsson <smackdh@gmail.com>
2024-01-21 09:18:49 +0900

    test

--- api/app/settings/staging.py
+++ api/app/settings/staging.py
@@ -8,14 +8,25 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")  # "team-sinta"
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")  # "eu-west-1"
+AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
 AWS_S3_FILE_OVERWRITE = False
 AWS_QUERYSTRING_AUTH = False
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_LOCATION='static'
+STATICFILES_DIRS = [
+        'static',
+    ]
+
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 # Static and Media
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/staticfiles/static/"
-MEDIA_URL = "/staticfiles/media/"
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_URL = "/staticfiles/static/"
+# MEDIA_URL = "/staticfiles/media/"
 
 # HTTPS Settings
 SESSION_COOKIE_SECURE = True
@@ -31,7 +42,6 @@ SECURE_HSTS_RELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 # Allowed Hosts
-
 ALLOWED_HOSTS_ENV = os.environ.get("ALLOWED_HOSTS")
 ALLOWED_HOSTS = ALLOWED_HOSTS_ENV.split(",") if ALLOWED_HOSTS_ENV else []
 print(ALLOWED_HOSTS)
