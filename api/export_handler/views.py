5eab2368621a8f64c8631846793afd0b9cb7a492
Mattias <mattiaswelamsson@gmail.com>
2024-02-22 22:49:06 +0900

    asd

--- api/export_handler/views.py
+++ api/export_handler/views.py
@@ -4,8 +4,6 @@ from django.http import FileResponse
 from reportlab.pdfgen import canvas
 from rest_framework.views import APIView
 
-from user.models import CustomUser
-
 
 class ExportToPdf(APIView):
     def get(self, request):
@@ -27,29 +25,38 @@ class ExportToPdf(APIView):
         # Create the PDF object, using the buffer as its "file."
         p = canvas.Canvas(buffer)
         row_height = 20
-        column_width = 400
-        data = [
-            ["ID", "Username", "Email"],
+        column_width = 140
+
+        # Dummy Data
+
+        dummy_interviewers = [
+            {"first_name": "Pablo", "last_name": "Escabar", "email": "LetsRead50ShadesOfGrey@slimy.now"},
+            {"first_name": "Mo", "last_name": "Swag", "email": "IcanTrustThisGuy@letsgo.com"},
+            {"first_name": "Henock", "last_name": "Yolo", "email": "IcanTrustThisGuy@letsgo.com"},
         ]
+        interviewer_data_list = []
+
+        for interviewer in dummy_interviewers:
+            interviewer_data = [interviewer["first_name"], interviewer["last_name"], interviewer["email"]]
+            interviewer_data_list.append(interviewer_data)
+
+        start_pos_x = 50
+        start_pos_y = 750
+        for row in interviewer_data_list:
+            for item in row:
+                p.drawString(start_pos_x, start_pos_y, str(item))
+                start_pos_x += column_width
+            start_pos_x = 50
+            start_pos_y -= row_height
 
         interview_information_data = [["position_title", "organization_name"]]
-        interviewer_data = [["first_name", "last_name", "email"]]
         conclusion_notes_data = [["author", "note"]]
         analytics_data = [["questions_solved", "time_past", "average_rating"]]
 
         # Document Top Title
         p.drawString(260, 800, "Sinta Export To Pdf")
 
-        for obj in CustomUser.objects.all():
-            data.append([obj.id, obj.username, obj.email])
-        start_pos_x = 50
-        start_pos_y = 750
-        for row in data:
-            for item in row:
-                p.drawString(start_pos_x, start_pos_y, str(item))
-                start_pos_x += column_width
-            start_pos_x = 50
-            start_pos_y -= row_height
+        # Process dummy interviewers
 
         # Close the PDF object cleanly, and we're done.
         p.showPage()
