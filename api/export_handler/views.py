42cb0744c2823c89dd06da2af30ed131711d93d8
Mattias Velamsson <smackdh@gmail.com>
2024-02-22 21:19:14 +0900

    add comment

--- api/export_handler/views.py
+++ api/export_handler/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+import io
+
+from django.http import FileResponse
 from reportlab.pdfgen import canvas
 from rest_framework.views import APIView
 
@@ -7,9 +9,7 @@ from user.models import CustomUser
 
 class ExportToPdf(APIView):
     def get(self, request):
-        response = HttpResponse(content_type="application/pdf")
-        response["Content-Disposition"] = 'attachment; filename="mydata.pdf"'
-
+        buffer = io.BytesIO()
         # TODO:
         # - Title: Conclusion
         # - Time
@@ -25,30 +25,35 @@ class ExportToPdf(APIView):
         # buffer = io.BytesIO)
 
         # Create the PDF object, using the buffer as its "file."
-        p = canvas.Canvas(response)
+        p = canvas.Canvas(buffer)
         row_height = 20
         column_width = 400
-
         data = [
             ["ID", "Username", "Email"],
         ]
 
+        interview_information_data = [["position_title", "organization_name"]]
+        interviewer_data = [["first_name", "last_name", "email"]]
+        conclusion_notes_data = [["author", "note"]]
+        analytics_data = [["questions_solved", "time_past", "average_rating"]]
+
+        # Document Top Title
         p.drawString(260, 800, "Sinta Export To Pdf")
 
         for obj in CustomUser.objects.all():
             data.append([obj.id, obj.username, obj.email])
-        # Draw the table
-        x = 50
-        y = 750
+        start_pos_x = 50
+        start_pos_y = 750
         for row in data:
             for item in row:
-                p.drawString(x, y, str(item))
-                x += column_width
-            x = 50
-            y -= row_height
+                p.drawString(start_pos_x, start_pos_y, str(item))
+                start_pos_x += column_width
+            start_pos_x = 50
+            start_pos_y -= row_height
 
         # Close the PDF object cleanly, and we're done.
         p.showPage()
         p.save()
 
-        return response
+        buffer.seek(0)
+        return FileResponse(buffer, as_attachment=True, filename="conclusion.pdf")
