660154191548ea4239d9c724add4ece08b087370
Mattias <mattiaswelamsson@gmail.com>
2024-03-01 05:43:11 +0900

    refactored competency and questions

--- api/export_handler/views.py
+++ api/export_handler/views.py
@@ -27,42 +27,46 @@ class ExportToPdf(APIView):
         3: "https://sinta-media.s3.eu-west-1.amazonaws.com/icons/5_competency_score.png",
     }
 
+    def get_icon_url(self, score, mapping_type="competency"):
+        if mapping_type == "competency":
+            return self.review_icon_mapping.get(score, "")
+        else:
+            return self.hire_decision_icon.get(score, "")
+
     def get_unique_file_name(self, interview_round_title):
         timestamp = datetime.now().strftime("%Y%m%d")
         filename = f"{interview_round_title.replace(' ', '_').lower()}_{timestamp}.pdf"
         return filename
 
+    def get_questions_and_answers(self, interview_round):
+        questions_and_answers = []
+        interview_questions = InterviewRoundQuestion.objects.filter(interview_round=interview_round)
+        for question in interview_questions:
+            answer = get_object_or_404(Answer, question=question)
+            questions_and_answers.append({"question": question, "answer": answer})
+        return questions_and_answers
+
+    def get_competency_and_review(self, interview_round):
+        competency_and_reviews = []
+        interview_questions = InterviewRoundQuestion.objects.filter(interview_round=interview_round)
+        for question in interview_questions:
+            competency = question.question.question.competency
+            review_value = question.question.question.review
+            icon_url = self.get_icon_url(review_value, "competency")
+            competency_and_reviews.append({"text": competency, "icon_url": icon_url})
+        return competency_and_reviews
+
     def get(self, request, *args, **kwargs):
-        print("Interview Round: ", request.data.get("interview_round_id"))
         interview_round_id = request.data.get("interview_round_id")
         interview_round = get_object_or_404(InterviewRound, id=interview_round_id)
-        print(interview_round)
-        interview_questions = InterviewRoundQuestion.objects.filter(interview_round=interview_round)
-        print(interview_questions)
         template = get_object_or_404(Template, id=interview_round.template_id)
-        print(template)
         candidate = interview_round.candidate
         interviewer = interview_round.interviewer
         interviewer_feedback = get_object_or_404(InterviewerFeedback, interview_round=interview_round)
-        print(interviewer_feedback)
         summary = get_object_or_404(Summary, interview_round=interview_round)
-        print(summary)
 
         unique_filename = self.get_unique_file_name(interview_round.title)
-        competency_and_reviews = []
-        questions_and_answers = []
         interviewer_hire_choice = 3
-
-        for question in interview_questions:
-            review_value = question.question.question.review
-            print("Review Value ", review_value)
-            icon_url = self.review_icon_mapping.get(review_value, "")
-            competency = question.question.question.competency
-            print("Icon Url ", icon_url)
-            print("Competency: ", competency)
-            competency_and_reviews.append({"text": competency, "icon_url": icon_url})
-            answer = get_object_or_404(Answer, question=question)
-            questions_and_answers.append({"question": question, "answer": answer})
 
         if interviewer_hire_choice == 1:
             decision_text = "Hire"
@@ -77,8 +81,8 @@ class ExportToPdf(APIView):
 
         context = {
             "interview_round": interview_round,
-            "questions_and_answers": questions_and_answers,
-            "competency_and_reviews": competency_and_reviews,
+            "questions_and_answers": self.get_questions_and_answers(interview_round),
+            "competency_and_reviews": self.get_competency_and_review(interview_round),
             "template": template,
             "interviewer": interviewer,
             "interviewer_feedback": interviewer_feedback,
